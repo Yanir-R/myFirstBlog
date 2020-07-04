0818862b8e9d75f3f0500b2fe39ded20099ebdca
Sheba467 <lowyouone@gmail.com>
2020-07-04 21:45:05 +0300

    adding not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ArticleListComponent } from './compoenents/article-list/article-list.component';
 import { AboutComponent } from './compoenents/about/about.component';
 import { ArticleComponent } from './compoenents/article/article.component';
+import { NotFoundComponent } from './compoenents/not-found/not-found.component';
 
 const routes: Routes = [
 	{ path: 'articles', component: ArticleListComponent },
 	{ path: 'about', component: AboutComponent },
+	{ path: '404', component: NotFoundComponent },
 	{ path: '', component: ArticleListComponent },
 	{ path: ':key', component: ArticleComponent }
-
 ];
 
 @NgModule({
--- src/app/compoenents/article/article.component.ts
+++ src/app/compoenents/article/article.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/article';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ArticleService } from 'src/app/article.service';
 
 @Component({
-  selector: 'app-article',
-  templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+	selector: 'app-article',
+	templateUrl: './article.component.html',
+	styleUrls: [ './article.component.css' ]
 })
 export class ArticleComponent implements OnInit {
+	article: Article = new Article();
+	constructor(private route: ActivatedRoute, private articleService: ArticleService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.route.params.subscribe((params) => {
+			const key = params.key;
+			this.articleService.getArticle(key).subscribe((article) => {
+				if (article === undefined) {
+					this.router.navigateByUrl('404');
+				}
+				this.article = article;
+				console.log(this.article);
+			});
+		});
+	}
 }
